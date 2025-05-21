c712c6b36895abae52607e427c5a5f2dcc9c8244
Georg Ponosov <ponosov.qa@gmail.com>
2025-05-21 15:11:57 +0200

    Убрал комментарий

--- grpc_course_client.py
+++ grpc_course_client.py
@@ -1,20 +1,3 @@
-# import grpc
-#
-# from faker import Faker
-# import course_service_pb2
-# import course_service_pb2_grpc
-#
-# fake = Faker()
-#
-# # Генерируем UUID
-# fake_uuid = fake.uuid4()
-# print(f"Сгенерированный UUID: {fake_uuid}")
-#
-# channel_courses = grpc.insecure_channel("localhost:50051")
-# stub = course_service_pb2_grpc.CourseServiceStub(channel_courses)
-#
-# response = stub.GetCourse(course_service_pb2.GetCoursesRequest(course_id= f"{fake_uuid}"))
-
 
 import grpc
 import course_service_pb2
