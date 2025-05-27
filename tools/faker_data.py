9dc2183fdc54f3ac5fc009f9f5ccc75d3adc4350
Georg Ponosov <ponosov.qa@gmail.com>
2025-05-27 13:07:31 +0200

    Созал метод который генерит тетсовые данные

---
+++ tools/faker_data.py
@@ -0,0 +1,16 @@
+import time
+from faker import Faker
+
+fake = Faker("ru")
+
+def get_random_email() -> str:
+    return f"test.{time.time()}@example.com"
+
+
+def get_data() -> dict:
+    return {
+        "last_name": fake.last_name(),
+        "first": fake.first_name(),
+        "middle": fake.middle_name(),
+        "password": fake.uuid4()
+    }
