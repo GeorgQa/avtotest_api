495707424e763e2d07f9767ea254d97e66c788fb
Georg Ponosov <ponosov.qa@gmail.com>
2025-05-28 13:39:51 +0200

    Доработкали клиента для загрузки файлов , написали описание методов

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -1,14 +1,52 @@
+from importlib.metadata import files
+from typing import TypedDict
+
 from httpx import URL, Response
 
 from clients.api_client import APIClient
 
 
+class CreateFilesRequestDict(TypedDict):
+    """
+    Описание тела запроса на создание файла
+    """
+    filename: str
+    directory: str
+    upload_file: str
+
+
 class FilesClient(APIClient):
-    def create_files(self):
-        pass
+    """
+    Климент для работы с api  /api/v1/files/
+    """
+    def create_files_api(self, request:CreateFilesRequestDict) -> Response:
+        """
+        метод для создания файла
 
-    def get_files(self):
-        pass
+        :param request: словарь содержаший названия файла, директорию жля сохранения, путь до файла
+        :return: Отвтет от сервера в виде обьекта httpx.Resposne
+        """
+        return self.post(
+            "/api/v1/files/",
+            data=request,
+            #Путь будет на уровне словаря определяться т.е. он не захардкожен
+            files={"upload file": open(request['upload_file'], 'rb')}
+        )
 
-    def delete_files(self):
-        pass
+    def get_files_api(self, file_id:str) -> Response:
+        """
+        Метод для получения файла
+
+        :param file_id: укникальный индификатор файла
+        :return: Ответа от сервера в виде объекта httpx.Response
+        """
+        return self.client.get(f"/api/v1/files/{file_id}")
+
+    def delete_files_api(self, file_id:str) -> Response:
+        """
+        Метод, который удаляет файл
+
+        :param file_id: уникальный индификатор файла
+        :return: Ответ от сервера в виде объекта httx.Response
+        """
+        return  self.client.delete(f"/api/v1/files/{file_id}")
