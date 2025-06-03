fcd53c7c878b15a845620ee5c8f8834bfced16ff
Georg Ponosov <ponosov.qa@gmail.com>
2025-06-03 12:21:35 +0200

    Добавил тип данных constr

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,7 +1,6 @@
 import uuid
 
-from pydantic import BaseModel,  EmailStr, Field
-
+from pydantic import BaseModel, EmailStr, Field, constr
 
 """
 Модель данных пользователя
@@ -10,10 +9,10 @@ from pydantic import BaseModel,  EmailStr, Field
 """
 class UserSchema(BaseModel):
     id: str = Field(default_factory= lambda : str(uuid.uuid4()))
-    email: EmailStr
-    last_name: str = Field(alias="lastName")
-    first_name: str = Field(alias="firstName")
-    middle_name: str = Field(alias="middleName")
+    email: constr(min_length=1, max_length=250) = EmailStr
+    last_name: constr(min_length=1 , max_length= 50)  = Field(alias="lastName")
+    first_name: constr(min_length=1, max_length= 50) = Field(alias="firstName")
+    middle_name: constr(min_length=1, max_length=50) = Field(alias="middleName")
 
 """
 Модель запроса на создание пользователя
@@ -21,11 +20,11 @@ class UserSchema(BaseModel):
 :return Готовая модель для запроса на создание пользователя
 """
 class CreateUserRequestSchema(BaseModel):
-    email: EmailStr
-    password: str
-    last_name: str = Field(alias="lastName")
-    first_name: str = Field(alias="firstName")
-    middle_name: str = Field(alias="middleName")
+    email:constr(min_length=1,max_length=250) =  EmailStr
+    password: constr(min_length=1 , max_length=250)
+    last_name: constr(min_length=1, max_length=50) = Field(alias="lastName")
+    first_name: constr(min_length=1, max_length=50)  = Field(alias="firstName")
+    middle_name: constr(min_length=1, max_length=50)  = Field(alias="middleName")
 
 """
 Модель на получение ответа с данными созданного пользователя
