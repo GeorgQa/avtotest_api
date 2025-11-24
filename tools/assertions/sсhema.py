4599331e29f9bc6e6afb1626386a87ee22a5aef5
Ponosov Egor <ponosov.qa@gmail.com>
2025-11-24 21:52:55 +0200

    Исправил ошибку в schema

--- tools/assertions/sсhema.py
+++ tools/assertions/sсhema.py
@@ -1,18 +1,17 @@
- from typing import Any
+from typing import Any
 
- from jsonschema import validate
- from jsonschema.validators import Draft202012Validator
+from jsonschema import validate
+from jsonschema.validators import Draft202012Validator
 
-
-def validate_json_schema(instanse: Any, schema: dict) -> None:
-     """
-     Функция проверяет валидность JSON-объекта по указанному схеме.
-     :param instanse: JSON-объект для проверки
-     :param schema: JSON-схема для проверки
-     :raise jsonschema.exceptions.ValidationError: Если JSON-объект не соответствует схеме
-     """
-     validate(
-         schema=schema,
-         instance=instanse,
-         cls=Draft202012Validator.FORMAT_CHECKER
-     )
+def validate_json_schema(instance: Any, schema: dict) -> None:
+    """
+    Функция проверяет валидность JSON-объекта по указанному схеме.
+    :param instance: JSON-объект для проверки
+    :param schema: JSON-схема для проверки
+    :raise jsonschema.exceptions.ValidationError: Если JSON-объект не соответствует схеме
+    """
+    validate(
+        instance=instance,
+        schema=schema,
+        cls=Draft202012Validator  # ← Правильный параметр
+    )
