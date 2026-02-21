7d471a5afcf40bcabd4351af590203cfc2d5629f
Ponosov Egor <ponosov.qa@gmail.com>
2026-02-21 18:16:54 +0300

    Добавил фикстуру function_user Применил её в тесты  test_create_user_and_get_it и test_login

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,11 +1,9 @@
 from httpx import Response
 
 from clients.api_client import APIClient
-from clients.authentication.authentication_schema import (
-    LoginRequestSchema,
-    LoginResponseSchema,
-    RefreshRequestSchema,
-)
+from clients.authentication.authentication_schema import (LoginRequestSchema,
+                                                          LoginResponseSchema,
+                                                          RefreshRequestSchema)
 from clients.public_http_builder import get_public_http_client
 
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,10 +2,14 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import get_private_users_client
-from clients.users.public_users_client import get_public_users_client
-from clients.users.user_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.public_users_client import (PublicUsersClient,
+                                               get_public_users_client)
+from clients.users.user_schema import (CreateUserRequestSchema,
+                                       CreateUserResponseSchema)
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_equal, assert_status_code
 from tools.assertions.sсhema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
@@ -13,51 +17,43 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
+def test_create_user(public_users_client: PublicUsersClient):
     """
     Тест создания нового юзера
     """
-    public_users_client = get_public_users_client()
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
-    # Валидация схемы body ответа
+
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
-    # Проверка статус кода
-    assert_status_code(response.status_code, HTTPStatus.OK)
+
     # Проверка данных
     assert_create_user_response(request=request, response=response_data)
 
     # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
     validate_json_schema(response.json(), response_data.model_json_schema())
 
+
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user_and_get_it():
+def test_create_user_and_get_it(
+    public_users_client: PublicUsersClient,
+    authentication_client: AuthenticationClient,
+    function_user: UserFixture,
+):
     """
     Тест для проверки авторизации и получения информации о новом пользовательтеле
     """
 
-    public_client = get_public_users_client()
-    create_request = CreateUserRequestSchema()
-    create_response = public_client.create_user_api(create_request)
-    assert_status_code(create_response.status_code, HTTPStatus.OK)
-    create_response_data = CreateUserResponseSchema.model_validate_json(
-        create_response.text
+    assert_create_user_response(
+        request=function_user.request, response=function_user.response
+    )
+    user_id = function_user.response.user.id
+
+    authentication_client = AuthenticationUserSchema(
+        email=function_user.request.email, password=function_user.request.password
     )
 
-    assert_create_user_response(request=create_request, response=create_response_data)
-    create_response_data = CreateUserResponseSchema.model_validate_json(
-        create_response.text
-    )
-    assert_status_code(create_response.status_code, HTTPStatus.OK)
-
-    user_id = create_response_data.user.id
-
-    auth_user = AuthenticationUserSchema(
-        email=create_request.email, password=create_request.password
-    )
-
-    private_client = get_private_users_client(auth_user)
+    private_client = get_private_users_client(authentication_client)
 
     get_response = private_client.get_user_api(user_id)
     assert_status_code(get_response.status_code, HTTPStatus.OK)
@@ -65,13 +61,21 @@ def test_create_user_and_get_it():
     get_response_data = CreateUserResponseSchema.model_validate_json(get_response.text)
 
     assert_equal(get_response_data.user.id, user_id, "id")
-    assert_equal(get_response_data.user.email, create_request.email, "email")
     assert_equal(
-        get_response_data.user.first_name, create_request.first_name, "first name"
+        get_response_data.user.email, function_user.response.user.email, "email"
     )
     assert_equal(
-        get_response_data.user.last_name, create_request.last_name, "last name"
+        get_response_data.user.first_name,
+        function_user.response.user.first_name,
+        "first name",
     )
     assert_equal(
-        get_response_data.user.middle_name, create_request.middle_name, "middle name"
+        get_response_data.user.last_name,
+        function_user.response.user.last_name,
+        "last name",
+    )
+    assert_equal(
+        get_response_data.user.middle_name,
+        function_user.response.user.middle_name,
+        "middle name",
     )
