9c952e70cd153794e2ed47bdc625f78da0dbe80f
Ponosov Egor <ponosov.qa@gmail.com>
2026-02-23 17:36:43 +0300

    Добавил в индекс гит

---
+++ tests/test_mark.py
@@ -0,0 +1,56 @@
+import pytest
+
+# @pytest.mark.api
+# class TestUserInterface:
+#
+#     @pytest.mark.smoke
+#     @pytest.mark.critical
+#     def test_login(self):
+#         pass
+#
+#     @pytest.mark.regression
+#     def test_forgot_password(self):
+#         pass
+#
+#     @pytest.mark.smoke
+#     def test_signup(self):
+#         pass
+#
+#
+# @pytest.mark.smoke
+# class TestLogin:
+#     @pytest.mark.smoke
+#     def test_valid_login(self):
+#         pass
+#
+#     @pytest.mark.regression
+#     def test_invalid_login(self):
+#         pass
+#
+# @pytest.mark.regression
+# class TestRegistration:
+#     @pytest.mark.regression
+#     def test_valid_registration(self):
+#         pass
+#
+#     @pytest.mark.smoke
+#     def test_invalid_registration(self):
+#         pass
+#
+# @pytest.mark.smoke
+# @pytest.mark.regression
+# class TestCheckout:
+#     @pytest.mark.smoke
+#     @pytest.mark.regression
+#     def test_valid_checkout(self):
+#         pass
+#
+#     def test_invalid_checkout(self):
+#         pass
+#
+# def test_search():
+#     pass
+
+# pytest -m "api and regression"
+# pytest -m "api"
+# В Pytest имеют иерархию возможно помечать тесты по разным маркерам внутри одного класса , чтобы удобнее было запускать
