af1415b46462a5902c72c49245640cefaa47e6dc
Ponosov Egor <ponosov.qa@gmail.com>
2026-03-06 13:02:37 +0300

    Добавил окружение в allure отчёт

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-from pydantic import HttpUrl, BaseModel, FilePath
+from typing import Self
+
+from pydantic import HttpUrl, BaseModel, FilePath, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -20,6 +22,7 @@ class Settings(BaseSettings):
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
+    allure_results_dir: DirectoryPath
 
     model_config = SettingsConfigDict(
         env_file=".env.local",
@@ -27,4 +30,12 @@ class Settings(BaseSettings):
         env_nested_delimiter="."
     )
 
-settings = Settings()
+    @classmethod
+    def initialize(cls) -> Self:  # Возвращает экземпляр класса Settings
+        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
+        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
+
+        # Передаем allure_results_dir в инициализацию настроек
+        return Settings(allure_results_dir=allure_results_dir)
+
+settings = Settings.initialize()
