eab2e3cadbfe1ee6505ec84942ae111c0ae4e4ac
Ponosov Egor <ponosov.qa@gmail.com>
2026-03-09 13:37:09 +0300

    обновил иницализицию настрйоек

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Self
-from pydantic import BaseModel, HttpUrl, FilePath
+from pydantic import BaseModel, HttpUrl, FilePath, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,7 +10,7 @@ class HTTPClientConfig(BaseModel):
 
     @property
     def client_url(self) -> str:
-        return str(self.url)
+        return str(self.url).rstrip('/')
 
 
 class TestDataConfig(BaseModel):
@@ -19,19 +19,27 @@ class TestDataConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env.local",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
     )
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
-    allure_results_dir: Path = Path("./allure-results")
+    allure_results_dir: DirectoryPath
 
-    def model_post_init(self, __context) -> None:
+    # Добавили метод initialize
+    @classmethod
+    def initialize(cls) -> Self:  # Возвращает экземпляр класса Settings
+        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
+        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
 
-        self.allure_results_dir.mkdir(exist_ok=True, parents=True)
+        # Передаем allure_results_dir в инициализацию настроек
+        return Settings(
+            allure_results_dir=allure_results_dir,
+            test_data=TestDataConfig(image_png_file="./testdata/files/image.png"),
+            http_client=HTTPClientConfig(url="http://localhost:8000", timeout=4.0)
+        )
 
 
 # Глобальный экземпляр настроек
-settings = Settings()
+settings = Settings.initialize()
