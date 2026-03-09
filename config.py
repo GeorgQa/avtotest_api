9941b9128ba87a26c73db37c73d083872437e833
Ponosov Egor <ponosov.qa@gmail.com>
2026-03-09 13:10:50 +0300

    Update Path in Settings

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
+from pathlib import Path
 from typing import Self
-
-from pydantic import BaseModel, HttpUrl, FilePath, DirectoryPath
+from pydantic import BaseModel, HttpUrl, FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -14,30 +14,24 @@ class HTTPClientConfig(BaseModel):
 
 
 class TestDataConfig(BaseModel):
-    image_png_file: str
+    image_png_file: FilePath
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        env_file=".env.local",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
     )
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
-    allure_results_dir: DirectoryPath  # Добавили новое поле
+    allure_results_dir: Path = Path("./allure-results")
 
-    @classmethod
-    def initialize(cls) -> Self:  # Возвращает экземпляр класса Settings
-        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
-        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
-        image_dir = TestDataConfig(image_png_file="./testdata/files/image.png")
+    def model_post_init(self, __context) -> None:
 
-        # Передаем allure_results_dir в инициализацию настроек
-        return Settings(allure_results_dir=allure_results_dir,
-                        test_data=image_dir,
-                        http_client=HTTPClientConfig(url='http://localhost:8000', timeout=4))
+        self.allure_results_dir.mkdir(exist_ok=True, parents=True)
 
 
-# Теперь вызываем метод initialize
-settings = Settings.initialize()
+# Глобальный экземпляр настроек
+settings = Settings()
