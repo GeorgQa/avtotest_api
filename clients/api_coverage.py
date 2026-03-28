5b428e679eb2e3a4ff4777c38675b3592c1e054c
Ponosov Egor <ponosov.qa@gmail.com>
2026-03-28 09:39:57 +0300

    up

--- clients/api_coverage.py
+++ clients/api_coverage.py
@@ -1,7 +1,6 @@
-from swagger_coverage_config import get_settings
-from swagger_coverage_tool.src.tracker.core import SwaggerCoverageTracker
+from swagger_coverage_tool import SwaggerCoverageTracker
+from swagger_coverage_tool.config import get_settings
 
-
-settings = get_settings(config_path=".swagger-coverage.yaml")
+settings = get_settings()
 
 tracker = SwaggerCoverageTracker(service="api-course", settings=settings)
